722fa7f390c1dbc8bd9d22b2dc107bedfc9e0da9
klaus <nickolai.beloglazov@gmail.com>
2021-12-10 19:28:35 +0600

    service-generator-maven-plugin::plugin-dev::add units registry

--- src/main/java/com/home/servicegenerator/plugin/processing/registry/ProjectUnitsRegistry.java
+++ src/main/java/com/home/servicegenerator/plugin/processing/registry/ProjectUnitsRegistry.java
@@ -2,10 +2,7 @@ package com.home.servicegenerator.plugin.processing.registry;
 
 import com.github.javaparser.ast.CompilationUnit;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class ProjectUnitsRegistry {
     private static final Map<String, CompilationUnit> PROJECT_UNITS_REGISTRY = Collections.synchronizedMap(new HashMap<>());
@@ -17,11 +14,23 @@ public class ProjectUnitsRegistry {
         synchronized (PROJECT_UNITS_REGISTRY) {
             Objects.requireNonNull(unit, "Unit must not be null");
             var _unit = unit.clone();
-            if (_unit.getPrimaryTypeName().isEmpty()) {
-                throw new IllegalArgumentException("Unit has not primary type");
+            if (_unit.getPrimaryTypeName().isEmpty() || _unit.getPrimaryTypeName().get().isEmpty()) {
+                throw new IllegalArgumentException("Unit id must not be empty");
             }
             return PROJECT_UNITS_REGISTRY.put(_unit.getPrimaryTypeName().get(), _unit);
         }
+    }
+
+    public static void registerAll(final Collection<CompilationUnit> units) {
+        units.stream()
+                .filter(u -> Objects.nonNull(u) && u.getPrimaryTypeName().isPresent() && !u.getPrimaryTypeName().get().isEmpty())
+                .forEach(ProjectUnitsRegistry::register);
+    }
+
+    public static void registerAll(final CompilationUnit... units) {
+        Arrays.stream(units)
+                .filter(u -> Objects.nonNull(u) && u.getPrimaryTypeName().isPresent() && !u.getPrimaryTypeName().get().isEmpty())
+                .forEach(ProjectUnitsRegistry::register);
     }
 
     public static CompilationUnit lookup(final String unitId) {
